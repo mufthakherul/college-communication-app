d59034c9dc5563250cb20492b86fec0fa1972a38
MD MUFTHAKHERUL ISLAM MIRAZ <130831166+mufthakherul@users.noreply.github.com>
2025-09-24 12:29:31 +0000

    Clean up: Remove node_modules and build files from repository
    Co-authored-by: mufthakherul <130831166+mufthakherul@users.noreply.github.com>

--- functions/lib/adminApproval.d.ts
+++
@@ -1,3 +0,0 @@
-import * as functions from "firebase-functions";
-export declare const requestAdminApproval: functions.HttpsFunction & functions.Runnable<any>;
-export declare const processApprovalRequest: functions.HttpsFunction & functions.Runnable<any>;
